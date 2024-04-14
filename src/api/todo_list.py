fee305dc19242df247d25a570aed1178e23ce0fe
Igor <select12345@yandex.ru>
2024-04-14 16:06:04 +0700

    init

---
+++ src/api/todo_list.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.get('/lists')
+async def get_lists():
+    return
+
+@router.post('/lists')
+async def create_lists():
+    return
+
+@router.delete('/lists/{list_id')
+async def delete_list():
+    return
