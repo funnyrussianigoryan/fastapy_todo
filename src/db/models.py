beaadab72ccbc446efb151e34a48fc21fc7e7ba0
Igor <select12345@yandex.ru>
2024-04-15 22:08:41 +0700

    start of creating db tables

---
+++ src/db/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Boolean, Column, String, UUID, DateTime, func, timezone, ForeignKey
+
+Base = declarative_base()
+
+
+class TodoList(Base):
+    __tablename__ = 'todo_list'
+
+    id = Column(UUID, primary_key=True, nullable=False)
+    title = Column(String(50), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
+
+class TodoItem(Base):
+    __tablename__ = 'todo_item'
+
+    id = Column(UUID, primary_key=True, nullable=False)
+    title = Column(String(50), nullable=False)
+    list_id = Column(ForeignKey(TodoList.id), nullable=False)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, server_default=func.now(tz=timezone.utc))
